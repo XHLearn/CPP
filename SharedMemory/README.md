266fa30e4569a3ff2b3ee74c68858cf7f64684b5
lamborghini-ubuntu <1323242382@qq.com>
2019-02-22 20:47:25 +0800

    共享内存

---
+++ SharedMemory/README.md
@@ -0,0 +1,1 @@
+[共享内存](https://www.ibm.com/developerworks/cn/linux/l-ipc/part5/index1.html)
---
+++ SharedMemory/shared2.cpp
@@ -0,0 +1,32 @@
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <cstring>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+typedef struct shared1
+{
+    char name[4];
+    int age;
+} people;
+
+int main(int argc, char **argv)
+{
+    int fd, i;
+    people *p;
+
+    fd = open(argv[1], O_CREAT | O_RDWR, 00777);
+
+    p = (people *)mmap(NULL, sizeof(people) * 10, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("name:%s age:%d\n", (*(p + i)).name, (*(p + i)).age);
+    }
+    munmap(p, sizeof(people) * 10);
+
+    return 0;
+}
